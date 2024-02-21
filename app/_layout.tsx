524fbf01d9b567b78a198639c37749526a938920
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2024-02-20 19:29:12 -0700

    feat: added search record screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,12 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import { useColorScheme } from "@/components/useColorScheme";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -19,7 +14,7 @@ export {
 } from "expo-router";
 
 export const unstable_settings = {
-  initialRouteName: "(auth)",
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -50,18 +45,47 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  const router = useRouter();
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <SafeAreaProvider>
-          <Stack>
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          </Stack>
-        </SafeAreaProvider>
-      </ThemeProvider>
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="searchRecord"
+            options={{
+              title: "Search",
+              headerShadowVisible: false,
+              headerStyle: { backgroundColor: "transparent" },
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Ionicons
+                    name="chevron-back-outline"
+                    size={30}
+                    color="black"
+                  />
+                </TouchableOpacity>
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="addRecord"
+            options={{
+              title: "Add Children Record",
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Ionicons
+                    name="chevron-back-outline"
+                    size={30}
+                    color="black"
+                  />
+                </TouchableOpacity>
+              ),
+            }}
+          />
+        </Stack>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
---
+++ app/searchRecord.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet, View, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import Page from "@/components/Page";
+import Colors from "@/constants/Colors";
+
+export default function Screen() {
+  return (
+    <Page style={{ flex: 1 }} headerShown={true}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Feather
+            name="search"
+            size={20}
+            color="#8E8E93"
+            style={styles.icon}
+          />
+          <TextInput
+            placeholder="Search name or record id"
+            placeholderTextColor="#8E8E93"
+            style={styles.input}
+          />
+        </View>
+      </View>
+    </Page>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "rgba(25, 154, 142, 0.25)",
+    borderRadius: 24,
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: "#000",
+  },
+});
