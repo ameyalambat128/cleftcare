c2bfd2106db7cca31ebef123c2c8da6dd347e0af
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2024-02-22 06:56:28 -0700

    feat: added os specific picker

--- app/(tabs)/(index)/addRecord.tsx
+++ app/(tabs)/(index)/addRecord.tsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Text,
   Modal,
+  Platform,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
@@ -14,9 +15,10 @@ import Colors from "@/constants/Colors";
 
 export default function Screen() {
   const [name, setName] = useState("");
-  const [birthDate, setBirthDate] = useState(new Date());
+  const [birthDate, setBirthDate] = useState<Date | null>(null);
   const [gender, setGender] = useState("");
   const [isPickerVisible, setPickerVisible] = useState(false);
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
   const [address, setAddress] = useState("");
   const [contactNumber, setContactNumber] = useState("");
 
@@ -36,7 +38,7 @@ export default function Screen() {
           <Feather name="user" size={20} color="#8E8E93" style={styles.icon} />
           <TextInput
             placeholder="Enter your name"
-            placeholderTextColor="#8E8E93"
+            placeholderTextColor={Colors.secondaryText}
             value={name}
             onChangeText={setName}
             style={styles.inputText}
@@ -56,50 +58,96 @@ export default function Screen() {
             color="#8E8E93"
             style={styles.icon}
           />
-          <Text style={styles.inputText}>{formatDate(birthDate)}</Text>
+          {birthDate ? (
+            <Text style={styles.inputText}>
+              {(birthDate.getMonth() + 1).toString().padStart(2, "0") +
+                "/" +
+                birthDate.getDate().toString().padStart(2, "0") +
+                "/" +
+                birthDate.getFullYear()}
+            </Text>
+          ) : (
+            <Text style={{ color: Colors.secondaryText }}>
+              Select Birth Date
+            </Text>
+          )}
         </TouchableOpacity>
 
         {/* Gender Picker */}
-        <TouchableOpacity
-          style={styles.inputField}
-          onPress={() => setPickerVisible(!isPickerVisible)}
-        >
-          <Feather name="user" size={20} color="#8E8E93" style={styles.icon} />
-          <Text style={styles.inputText}>
-            {gender ? gender : "Select Gender"}
-          </Text>
-        </TouchableOpacity>
-        <Modal
-          visible={isPickerVisible}
-          transparent={true}
-          animationType="slide"
-          onRequestClose={() => setPickerVisible(!isPickerVisible)}
-        >
-          <View style={styles.modalOverlay}>
-            <View style={styles.pickerContainer}>
-              <Picker
-                selectedValue={gender}
-                onValueChange={(itemValue, itemIndex) => {
-                  setGender(itemValue);
-                  setPickerVisible(!isPickerVisible);
-                }}
-                style={styles.picker}
-              >
-                <Picker.Item label="Select Gender" value="" />
-                <Picker.Item label="Male" value="Male" />
-                <Picker.Item label="Female" value="Female" />
-                <Picker.Item label="Other" value="Other" />
-              </Picker>
-            </View>
-          </View>
-        </Modal>
+        {Platform.OS === "android" && (
+          <TouchableOpacity
+            style={styles.dateInputField}
+            onPress={() => setPickerVisible(!isPickerVisible)}
+          >
+            <Feather name="user" size={20} color="#8E8E93" />
+
+            <Picker
+              selectedValue={gender}
+              mode="dropdown"
+              onValueChange={(itemValue, itemIndex) => {
+                setGender(itemValue);
+                setPickerVisible(!isPickerVisible);
+              }}
+              style={styles.picker}
+            >
+              <Picker.Item
+                label="Select Gender"
+                style={{ color: Colors.secondaryText }}
+                value=""
+              />
+              <Picker.Item label="Male" value="Male" />
+              <Picker.Item label="Female" value="Female" />
+              <Picker.Item label="Other" value="Other" />
+            </Picker>
+          </TouchableOpacity>
+        )}
+        {Platform.OS === "ios" && (
+          <>
+            <TouchableOpacity
+              style={styles.inputField}
+              onPress={() => setPickerVisible(!isPickerVisible)}
+            >
+              <Feather
+                name="user"
+                size={20}
+                color="#8E8E93"
+                style={styles.icon}
+              />
+              <Text style={{ color: Colors.secondaryText }}>Select Gender</Text>
+            </TouchableOpacity>
+            <Modal
+              visible={isPickerVisible}
+              transparent={true}
+              animationType="slide"
+              onRequestClose={() => setPickerVisible(!isPickerVisible)}
+            >
+              <View style={styles.modalOverlay}>
+                <View style={styles.pickerContainer}>
+                  <Picker
+                    selectedValue={gender}
+                    onValueChange={(itemValue, itemIndex) => {
+                      setGender(itemValue);
+                      setPickerVisible(!isPickerVisible);
+                    }}
+                    style={styles.picker}
+                  >
+                    <Picker.Item label="Select Gender" value="" />
+                    <Picker.Item label="Male" value="Male" />
+                    <Picker.Item label="Female" value="Female" />
+                    <Picker.Item label="Other" value="Other" />
+                  </Picker>
+                </View>
+              </View>
+            </Modal>
+          </>
+        )}
 
         {/* Address Input */}
         <View style={styles.inputField}>
           <Feather name="home" size={20} color="#8E8E93" style={styles.icon} />
           <TextInput
             placeholder="Enter your Address"
-            placeholderTextColor="#8E8E93"
+            placeholderTextColor={Colors.secondaryText}
             value={address}
             onChangeText={setAddress}
             style={styles.inputText}
@@ -111,7 +159,7 @@ export default function Screen() {
           <Feather name="phone" size={20} color="#8E8E93" style={styles.icon} />
           <TextInput
             placeholder="Contact number"
-            placeholderTextColor="#8E8E93"
+            placeholderTextColor={Colors.secondaryText}
             value={contactNumber}
             onChangeText={setContactNumber}
             style={styles.inputText}
@@ -123,11 +171,11 @@ export default function Screen() {
         <View style={styles.inputField}>
           <Feather
             name="paperclip"
-            size={20}
+            size={22}
             color="#8E8E93"
             style={styles.icon}
           />
-          <Text style={styles.inputText}>Attachment</Text>
+          <Text style={{ color: Colors.secondaryText }}>Attachment</Text>
           {/* Implement attachment functionality */}
         </View>
       </View>
@@ -141,29 +189,37 @@ const styles = StyleSheet.create({
   },
   headerText: {
     marginBottom: 20,
-    fontSize: 16,
+    fontSize: 14,
     textAlign: "center",
+    color: Colors.secondaryText,
   },
   inputField: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "#F0F0F5",
+    backgroundColor: Colors.background,
     borderRadius: 10,
     padding: 15,
+    marginTop: 10,
+  },
+  dateInputField: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.background,
+    borderRadius: 10,
+    paddingHorizontal: 15,
     marginTop: 10,
   },
   icon: {
     marginRight: 10,
   },
   inputText: {
-    flex: 1,
     color: "#000",
   },
   modalOverlay: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Dim the background
+    backgroundColor: "rgba(0, 0, 0, 0.3)", // Dim the background
   },
   pickerContainer: {
     backgroundColor: "white",
