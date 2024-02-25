3f4fb0e870777e62d25154d01e7f54cefa2b702b
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2024-02-25 00:14:00 -0700

    style: updated styling

--- app/(tabs)/tutorials.tsx
+++ app/(tabs)/tutorials.tsx
@@ -1,20 +1,31 @@
 import Page from "@/components/Page";
+import Colors from "@/constants/Colors";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Screen() {
   return (
-    <Page style={styles.container}>
-      <Text style={styles.title}>Tutorials</Text>
+    <Page
+      style={{ flex: 1, backgroundColor: Colors.background }}
+      headerShown={false}
+    >
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.title}>Tutorials</Text>
+        </View>
+      </View>
     </Page>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding: 20,
+  },
+  headerContainer: {
+    display: "flex",
+    flexDirection: "column",
     alignItems: "flex-start",
     justifyContent: "flex-start",
-    margin: 20,
   },
   title: {
     fontSize: 28,
