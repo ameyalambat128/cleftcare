3e80ddb86256193439e87972b7c399930c003e60
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2025-01-27 12:19:21 -0700

    fix: android invalid hearingStatus fixed

--- app/(tabs)/(index)/add-record/index.tsx
+++ app/(tabs)/(index)/add-record/index.tsx
@@ -382,8 +382,14 @@ export default function Screen() {
                   style={{ color: Colors.secondaryText }}
                   value=""
                 />
-                <Picker.Item label="Yes, I have hearing loss" value="Yes" />
-                <Picker.Item label="No, I have no hearing loss" value="No" />
+                <Picker.Item
+                  label="Yes, I have hearing loss"
+                  value="Yes, I have hearing loss"
+                />
+                <Picker.Item
+                  label="No, I have no hearing loss"
+                  value="No, I have no hearing loss"
+                />
               </Picker>
             </TouchableOpacity>
           )}
@@ -512,7 +518,7 @@ export default function Screen() {
               style={styles.icon}
             />
             <Text style={{ color: Colors.secondaryText }}>
-              Take/Choose Photo
+              Take/Choose Photo (Optional)
             </Text>
           </TouchableOpacity>
 
